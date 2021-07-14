3a841f3c4c1d27d14963d9db0369ef3b5f62462c
Kevin Grove <kevinwilsongrove@gmail.com>
2021-07-14 10:07:04 -0500

    Initial commit with completed technical assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,211 @@
-import logo from './logo.svg';
+import { ScaleLoader } from "react-spinners";
 import './App.css';
+import { fetchUrl, studentListItem, triggerDivider } from './helpers/helpers'
+import { useEffect, useState } from 'react';
+import { 
+  InputBase,
+  makeStyles,
+  fade,
+  Divider,
+  List
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  // styles the search bar components
+    inputRoot: {
+      color: 'inherit',
+      width: '100%'
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+    search: {
+      position: 'relative',
+      backgroundColor: fade(theme.palette.common.white, 1),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.85),
+      },
+      width: '100%',
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  })
+)
 
 function App() {
+  const classes = useStyles();
+  const [students, setStudents] = useState([])
+  const [searchName, setSearchName] = useState('')
+  const [searchTag, setSearchTag] = useState('')
+  const [valueAddTag, setValueAddTag] = useState('')
+  const [targetNode, setTargetNode] = useState(null)
+  const [tagsList, setTagsList] = useState([])
+  const url = "https://api.hatchways.io/assessment/students"
+
+  useEffect(async () => {
+    // fetches student data and adds a tag key with an empty array value
+    // then sets the mutated list as the students state
+    let response = await fetchUrl(url)
+    let newList = await response.students.map(
+      (s) => ({...s, tags: []})
+    )
+    setStudents(newList)
+  }, [])
+
+  useEffect(() => {
+    // this useEffect is listening to changes on valueAddTag and rerenders upon any changes to its state
+  }, [valueAddTag])
+  
+  const buildStudentsList = (name, tag) => {
+    // function listens to the search by name and search by tag inputs
+    // it builds a list according to the search criteria
+    // then calls the studentListItem function to build a component based on individual student's data
+    if (name === '' && tag === '') {
+      return ( 
+        students.map((s) => {
+          let grades = s.grades.map((i) => parseInt(i))
+          let gradeAvg = grades.reduce((a,b) => a + b, 0) / grades.length
+          let pos = students.indexOf(s) + 1
+              return studentListItem(s, gradeAvg, targetNode, setTargetNode, pos, valueAddTag, setValueAddTag, tagsList, setTagsList)
+            }
+          )
+      )
+    } else if (name !== '' && tag !== '') {
+      const res = students.filter(student => {
+        if (student.firstName.toLowerCase().startsWith(name) || student.lastName.toLowerCase().startsWith(name)) {
+          return student
+        } 
+      })
+      const newRes = res.filter(student => {
+        for (let i = 0; i < student.tags.length; i++) {
+          if (student.tags[i].startsWith(tag)) {
+            return student
+          }
+        }
+      })
+      return newRes.map((s) => {
+        let grades = s.grades.map((i) => parseInt(i))
+        let gradeAvg = grades.reduce((a,b) => a + b, 0) / grades.length
+        let pos = students.indexOf(s) + 1
+        return studentListItem(s, gradeAvg, targetNode, setTargetNode, pos, valueAddTag, setValueAddTag, tagsList, setTagsList)
+      })
+    } else if (name !== '') {
+        const res = students.filter(student => {
+          if (student.firstName.toLowerCase().startsWith(name) || student.lastName.toLowerCase().startsWith(name)) {
+            return student
+          } 
+        })
+        return res.map((s) => {
+          let grades = s.grades.map((i) => parseInt(i))
+          let gradeAvg = grades.reduce((a,b) => a + b, 0) / grades.length
+          let pos = students.indexOf(s) + 1
+          return studentListItem(s, gradeAvg, targetNode, setTargetNode, pos, valueAddTag, setValueAddTag, tagsList, setTagsList)
+        })
+    } else if (tag !== '') {
+        const res = students.filter(student => {
+          for (let i = 0; i < student.tags.length; i++) {
+            if (student.tags[i].startsWith(tag)) {
+              return student
+            }
+          }
+        })
+        return res.map((s) => {
+          let grades = s.grades.map((i) => parseInt(i))
+          let gradeAvg = grades.reduce((a,b) => a + b, 0) / grades.length
+          let pos = students.indexOf(s) + 1
+              return studentListItem(s, gradeAvg, targetNode, setTargetNode, pos, valueAddTag, setValueAddTag, tagsList, setTagsList)
+        })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      id='app'
+    >
+      {
+        students === [] ?
+        <>
+          <div id="loader">
+            <ScaleLoader
+              id='loader'
+              color={'#FFFFFF'}
+              height={90}
+              width={20}
+              radius={20}
+              margin={10}
+            />
+          </div>
+        </>
+        :
+        <div
+          id='main-container'
         >
-          Learn React
-        </a>
-      </header>
+          
+            <div 
+              className={classes.search}
+              id='search-bar-name'
+            >
+                <InputBase
+                  placeholder="Search by name"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  className='search-input raleway'
+                  inputProps={{ 'aria-label': 'search' }}
+                  onChange={e => 
+                    setSearchName(e.target.value)
+                  }
+                  onFocus={(e) => triggerDivider(e)}
+                  onBlur={(e) => triggerDivider(e)}
+                />
+            </div>
+            <Divider
+            className='input-divider'
+            />
+            <div 
+              className={classes.search}
+              id='search-bar-tag'
+            >
+                <InputBase
+                  placeholder="Search by tag"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  className='search-input raleway'
+                  inputProps={{ 'aria-label': 'search' }}
+                  onChange={e => setSearchTag(e.target.value)}
+                  onFocus={(e) => triggerDivider(e)}
+                  onBlur={(e) => triggerDivider(e)}
+                />
+            </div>
+            <Divider 
+            className='input-divider'
+            />
+            
+
+            <List
+              id='students-info-container'
+            >
+              { buildStudentsList(searchName, searchTag) }
+            </List>
+
+        </div>
+      }
     </div>
   );
 }
---
+++ src/helpers/helpers.js
@@ -0,0 +1,276 @@
+import {
+    ListItem,
+    Typography,
+    Divider,
+    Avatar,
+    InputBase,
+    IconButton
+} from '@material-ui/core'
+import {
+    Add,
+    Remove
+} from '@material-ui/icons'
+
+export const fetchUrl = async (url) => {
+    // fetches data from url and converts it into json
+    let response = await fetch(url)
+    response =  await response.json()
+    return response
+}
+
+export const triggerDivider = (e) => {
+    // this changes the divider background color below the search input
+    // depending on whether the input is focused on or has been exited
+    if (e.type === 'blur') {
+        document.querySelector('.input-divider-selected').classList = 'MuiDivider-root input-divider'
+    } else if (e.type === 'focus') {
+        e.target.parentElement.parentElement.nextElementSibling.classList = 'MuiDivider-root input-divider-selected'
+    }
+    return
+}
+
+export const studentListItem = (s, gradeAvg, targetNode, setTargetNode, pos, valueAddTag, setValueAddTag, tagsList, setTagsList) => {
+    // function builds and returns a list item component depending on individual student's data
+    const gradesContainer = document.querySelector('.grades-container')
+    
+    const toggleGrades = (s, node) => {
+        // function builds the student's individual grades list
+        // inserts it to the appropriate position on the student's list item component
+        const tagContainer = document.getElementById(`student-${pos}-tag-container`)
+        if (gradesContainer) {
+            gradesContainer.remove()
+        }
+        let mainDiv = document.createElement('div')
+        mainDiv.className = 'grades-container'
+        mainDiv.style.marginBottom = '20px'
+        let avatarFiller = document.createElement('div')
+        avatarFiller.className = 'student-avatar-filler'
+        mainDiv.appendChild(avatarFiller)
+        let studentInfoContainer = document.createElement('div')
+        studentInfoContainer.className = 'student-info-container'
+        let studentInfo = document.createElement('div')
+        studentInfo.className = 'student-info'
+        studentInfo.style.paddingLeft = '45px'
+        s.grades.map((grade) => {
+            let pos = s.grades.indexOf(grade)
+            let text = document.createElement('p')
+            text.className = 'MuiTypography-root info-text raleway MuiTypography-body1'
+            text.innerHTML = `Test ${pos + 1}: ${grade}%`
+            studentInfo.appendChild(text)
+        })
+        studentInfoContainer.appendChild(studentInfo)
+        mainDiv.appendChild(studentInfoContainer)
+        if (mainDiv.parentNode === node || tagContainer.parentNode === node) {
+            node.insertBefore(mainDiv, tagContainer)
+        }
+        return
+
+    }
+
+    const renderIcon = (pos, targetNode) => {
+        // function returns a different icon depending on if the dropdown grades toggle was clicked
+        let node = document.getElementById(`student-${pos}`)
+        if (node !== null) {
+            if (node.children[1] === gradesContainer) {
+                return (
+                    <>
+                        <IconButton
+                        size="small"
+                        style={{
+                            backgroundColor: "white"
+                        }}
+                        onClick={
+                            () => {
+                                setTargetNode(null)
+                                gradesContainer.remove()
+                            }
+                        }
+                        >
+                            <Remove 
+                            className="dropdown-icon"
+                            onClick={
+                                (e) => {
+                                    setTargetNode(null)
+                                    gradesContainer.remove()                            }
+                            }
+                            />
+                        </IconButton>
+                    </>
+                )
+            } else if (targetNode === null || targetNode !== node) {
+                return (
+                    <>
+                        <IconButton
+                        size="small"
+                        style={{
+                            backgroundColor: "white"
+                        }}
+                        onClick={
+                            (e) => {
+                                setTargetNode(e.target.parentNode.parentNode.parentNode.parentNode.parentNode)
+                                toggleGrades(s, e.target.parentNode.parentNode.parentNode.parentNode.parentNode)
+                            }
+                        }
+                        >
+                            <Add 
+                            className="dropdown-icon"
+                            onClick={
+                                (e) => {
+                                    setTargetNode(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+                                    toggleGrades(s, e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+                                }
+                            }
+                            />
+                        </IconButton>
+                    </>
+                )
+            }
+        } else {
+            return (
+                <>
+                    <IconButton
+                    size="small"
+                    style={{
+                        backgroundColor: "white"
+                    }}
+                    onClick={
+                        (e) => {
+                            setTargetNode(e.target.parentNode.parentNode.parentNode.parentNode.parentNode)
+                            toggleGrades(s, e.target.parentNode.parentNode.parentNode.parentNode.parentNode)
+                        }
+                    }
+                    >
+                        <Add 
+                        className="dropdown-icon"
+                        onClick={
+                            (e) => {
+                                setTargetNode(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+                                toggleGrades(s, e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+                            }
+                        }
+                        />
+                    </IconButton>
+                </>
+            )
+        }
+    }
+
+    return (
+        <>
+            <div
+            className='student-card-container'
+            id={`student-${pos}`}
+            >
+                <ListItem
+                className='student-card'
+                >
+                    <Avatar 
+                    alt={`${s.firstName} ${s.lastName}`} 
+                    src={s.pic}
+                    className='student-avatar'
+                    />
+                    <div
+                    className='student-info-container'
+                    >
+
+                        <Typography
+                        variant='h2'
+                        className='student-title raleway'
+                        >
+                            {s.firstName.toUpperCase() + ' ' + s.lastName.toUpperCase()}
+                        </Typography>
+
+                        <div
+                        className='student-info'
+                        >
+                            <Typography
+                            className='info-text raleway'
+                            >
+                            Email: {s.email}
+                            </Typography>
+                            <Typography
+                            className='info-text raleway'
+                            >
+                            Company: {s.company}
+                            </Typography>
+                            <Typography
+                            className='info-text raleway'
+                            >
+                            Skill: {s.skill}
+                            </Typography>
+                            <Typography
+                            className='info-text raleway'
+                            >
+                            Average: {gradeAvg}%
+                            </Typography>
+
+                        </div>
+                    </div>
+                    <div
+                    className="student-dropdown-icon-container"
+                    >
+                       {
+                           renderIcon(pos, targetNode)
+                       }
+                    </div>
+                </ListItem>
+                <div
+                    className='tag-container'
+                    id={`student-${pos}-tag-container`}
+                >
+                    <div
+                        className='tag-list-container'
+                        id={`student-${pos}-tags`}
+                    >
+                        {
+                            s.tags
+                            ?
+                            s.tags.map(tag => {
+                                return (
+                                    <>
+                                        <div
+                                        className='tag-box'
+                                        id={tag}
+                                        >
+                                            <Typography
+                                            className='info-text raleway'
+                                            style={{
+                                                padding: '5px 10px',
+                                                fontSize: '20px'
+                                            }}
+                                            >
+                                                {tag}
+                                            </Typography>
+                                        </div>
+                                    </>
+                                )
+                            })
+                            :
+                            null
+                        }
+                    </div>
+                    <InputBase
+                    placeholder="Add a tag"
+                    className='add-tag-input raleway'
+                    inputProps={{ 'aria-label': 'search' }}
+                    onChange={e => setValueAddTag(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            if (!s.tags.includes(valueAddTag)){
+                                s.tags.push(valueAddTag)
+                                e.target.value = ''
+                                setTagsList([...tagsList, valueAddTag])
+                            } else {
+                                e.target.value = ''
+                            }
+                        }
+                    }}
+                    />
+                </div>
+            </div>
+                <Divider 
+                className='student-divider'
+                />
+        </>
+    )
+}
